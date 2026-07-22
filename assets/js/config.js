c38c244c8e337617888eaca65e1ab8c7d3165820
used-clothes-korea <silverbirch0125@gmail.com>
2026-07-22 19:51:46 +0900

    Add files via upload

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+window.SITE_CONFIG={password:"TNF0125",kakaoId:"Korea Shipping",kakaoQr:"assets/images/kakao-qr-placeholder.svg",judressUrl:"https://judress.tsukuenoue.com/",pickupTimes:["10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"],pickupAppendMessage:"호텔 명함 사진과 짐 전체가 보이는 사진도 보내겠습니다.",shippingTemplate:`안녕하세요.
+일본으로 배송을 부탁드립니다.
+아래 정보를 확인해 주세요.
+
+■ 이름 (영문): {{name}}
+■ 우편번호: {{postal}}
+■ 주소 (영문): {{address}}
+■ 전화번호: {{phone}}
+
+감사합니다!`};
